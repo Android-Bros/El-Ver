acf90b48e7f32c45b8f649463f6016e7ed0b8625
adematici <adematicis41@gmail.com>
2021-11-27 02:25:59 +0300

    register screen updated

---
+++ app/src/main/java/com/androidbros/elver/presentation/ui/register/RegisterFragment.kt
@@ -0,0 +1,35 @@
+package com.androidbros.elver.presentation.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androidbros.elver.R
+import com.androidbros.elver.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
