5f4c2aef7a10b7e5782192b92dfa53a93d1c5417
adematici <adematicis41@gmail.com>
2021-11-27 04:16:49 +0300

    register fragment logic completed

--- app/src/main/java/com/androidbros/elver/UserModel.kt
+++ app/src/main/java/com/androidbros/elver/UserModel.kt
@@ -3,10 +3,10 @@ package com.androidbros.elver
 import com.google.firebase.Timestamp
 
 data class UserModel(
-    val email: String,
-    val password: String,
     val name: String,
     val surname: String,
+    val email: String,
+    val userUid: String,
     val phoneNumber: String,
     var profileImage: String?,
     var profileImageName: String?,
--- app/src/main/java/com/androidbros/elver/presentation/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/androidbros/elver/presentation/ui/register/RegisterFragment.kt
@@ -1,30 +1,223 @@
 package com.androidbros.elver.presentation.ui.register
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.androidbros.elver.R
 import com.androidbros.elver.databinding.FragmentRegisterBinding
+import com.androidbros.elver.presentation.ui.FlowActivity
+import com.google.android.material.snackbar.Snackbar
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: RegisterViewModel
+    private var selectedBitmap: Bitmap? = null
+    private var selectedUri: Uri? = null
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<out String>>
+    private val neededRuntimePermissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        registerLauncher()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(RegisterViewModel::class.java)
+
+        binding.imageViewUserProfile.setOnClickListener {
+            println("Tikladik")
+            if ((ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                        + ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ))
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                // izin verilmemis, izin gerekli
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(
+                        it,
+                        R.string.gallery_permission,
+                        Snackbar.LENGTH_LONG
+                    ).setAction(R.string.give_permission) {
+                        permissionLauncher.launch(neededRuntimePermissions)
+                    }.show()
+                } else {
+                    permissionLauncher.launch(neededRuntimePermissions)
+                }
+            } else {
+                // izin verilmis, galeriye gidis
+                val galleryIntent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(galleryIntent)
+            }
+        }
+
+        binding.buttonRegister.setOnClickListener {
+            if (binding.imageViewUserProfile.drawable != null) {
+                if (selectedBitmap != null) {
+                    firebaseDataSave(selectedUri)
+                } else {
+                    firebaseDataSave(null)
+                }
+            } else {
+                Toast.makeText(requireContext(), R.string.select_picture, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.textViewLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
     }
+
+    private fun firebaseDataSave(selectedImage: Uri?) {
+        if (dataControl()) {
+            val name = binding.editTextName.text.toString()
+            val surname = binding.editTextSurname.text.toString()
+            val email = binding.editTextMail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+            val passwordAgain = binding.editTextRepeatPassword.text.toString()
+            val phoneNumber = binding.editTextPhoneNumber.text.toString()
+            if (password == passwordAgain) {
+                viewModel.registrationConfirmationStatus(
+                    email,
+                    password,
+                    name,
+                    surname,
+                    phoneNumber,
+                    selectedImage,
+                    requireContext()
+                )
+                observeData()
+            } else {
+                Toast.makeText(requireContext(), R.string.password_must_match, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else {
+            Toast.makeText(requireContext(), R.string.fill_in_the_blanks, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun observeData() {
+        viewModel.animation.observe(viewLifecycleOwner, { animation ->
+            animation?.let {
+                if (it) {
+                    binding.scrollLayout.visibility = View.INVISIBLE
+                    binding.progressBar.visibility = View.VISIBLE
+                } else {
+                    binding.scrollLayout.visibility = View.VISIBLE
+                    binding.progressBar.visibility = View.GONE
+                }
+            }
+        })
+        viewModel.dataConfirmation.observe(viewLifecycleOwner, { dataConfirm ->
+            dataConfirm?.let { confirm ->
+                if (confirm) {
+                    activity?.let {
+                        Toast.makeText(
+                            requireContext(),
+                            "${resources.getString(R.string.welcome)} ${binding.editTextName.text}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        val intent = Intent(it, FlowActivity::class.java)
+                        it.startActivity(intent)
+                        it.finish()
+                    }
+                }
+            }
+        })
+    }
+
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        val imageData = intentFromResult.data
+                        selectedUri = imageData
+                        if (imageData != null) {
+                            try {
+                                if (Build.VERSION.SDK_INT >= 28) {
+                                    val source = ImageDecoder.createSource(
+                                        requireActivity().contentResolver,
+                                        imageData
+                                    )
+                                    selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                    binding.imageViewUserProfile.setImageBitmap(selectedBitmap)
+                                } else {
+                                    selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                        requireActivity().contentResolver,
+                                        imageData
+                                    )
+                                    binding.imageViewUserProfile.setImageBitmap(selectedBitmap)
+                                }
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
+            }
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach {
+                    if (it.value && it.key == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        val galleryIntent =
+                            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        activityResultLauncher.launch(galleryIntent)
+                    }
+                }
+            }
+    }
+
+    private fun dataControl(): Boolean = binding.editTextName.text.isNotEmpty()
+            && binding.editTextSurname.text.isNotEmpty() && binding.editTextMail.text.isNotEmpty() &&
+            binding.editTextPassword.text.isNotEmpty() && binding.editTextRepeatPassword.text.isNotEmpty()
+            && binding.editTextPhoneNumber.text.isNotEmpty()
 
     override fun onDestroyView() {
         super.onDestroyView()
